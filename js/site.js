50aac8dbb3d0d65131c2a9b7ccf8794ab1b1e66b
Ethan <ethanling1@gmail.com>
2018-11-11 23:05:23 -0500

    Fixed Nav

--- js/site.js
+++ js/site.js
@@ -1,15 +1,18 @@
 // Nav animation logic
 const nav = document.querySelector('#main-nav');
-const name = document.querySelector('#name');
+const name = document.querySelector('#about');
 
 let topOfName = name.offsetTop;
 console.log(topOfName);
 
 function fixNav() {
     if (window.scrollY > topOfName) {
+        document.body.style.paddingTop = nav.offsetHeight + 'px';
         document.body.classList.add('fixed-nav');
     } else {
         document.body.classList.remove('fixed-nav');
         document.body.style.paddingTop = 0;
     }
 }
+
+window.addEventListener('scroll', fixNav);
