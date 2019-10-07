d3a5e63f554c63f5cf8f6035f289c62287eda006
Ethan <ethanling1@gmail.com>
2019-10-07 13:30:10 -0400

    Removed nav

--- js/site.js
+++ js/site.js
@@ -1,19 +1,3 @@
-// Nav animation logic
-const nav = document.querySelector("#main-nav");
-const name = document.querySelector("#about");
-
-let topOfName = name.offsetTop;
-
-function fixNav() {
-    if (window.scrollY > topOfName) {
-        document.body.style.paddingTop = nav.offsetHeight + "px";
-        document.body.classList.add("fixed-nav");
-    } else {
-        document.body.classList.remove("fixed-nav");
-        document.body.style.paddingTop = 0;
-    }
-}
-
 // Square project list items
 function setProjectItemHeights() {
     const projectItems = document.getElementsByClassName("projects-item");
@@ -24,10 +8,8 @@ function setProjectItemHeights() {
     }
 }
 
-window.addEventListener("scroll", fixNav);
 window.addEventListener("resize", setProjectItemHeights);
 
 document.addEventListener("DOMContentLoaded", function() {
-    fixNav();
     setProjectItemHeights();
 });
