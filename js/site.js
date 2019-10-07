da1a28ac8e92ea97a4c70436bc78de9033651f43
Ethan <ethanling1@gmail.com>
2019-10-07 13:08:42 -0400

    Fixed project items safari bug

--- js/site.js
+++ js/site.js
@@ -1,18 +1,33 @@
 // Nav animation logic
-const nav = document.querySelector('#main-nav');
-const name = document.querySelector('#about');
+const nav = document.querySelector("#main-nav");
+const name = document.querySelector("#about");
 
 let topOfName = name.offsetTop;
-console.log(topOfName);
 
 function fixNav() {
     if (window.scrollY > topOfName) {
-        document.body.style.paddingTop = nav.offsetHeight + 'px';
-        document.body.classList.add('fixed-nav');
+        document.body.style.paddingTop = nav.offsetHeight + "px";
+        document.body.classList.add("fixed-nav");
     } else {
-        document.body.classList.remove('fixed-nav');
+        document.body.classList.remove("fixed-nav");
         document.body.style.paddingTop = 0;
     }
 }
 
-window.addEventListener('scroll', fixNav);
+// Square project list items
+function setProjectItemHeights() {
+    const projectItems = document.getElementsByClassName("projects-item");
+    console.log(projectItems);
+    for (let i=0; i < projectItems.length; i++) {
+        const width = projectItems[0].offsetWidth;
+        projectItems[i].style.height = width;
+    }
+}
+
+window.addEventListener("scroll", fixNav);
+window.addEventListener("resize", setProjectItemHeights);
+
+document.addEventListener("DOMContentLoaded", function() {
+    fixNav();
+    setProjectItemHeights();
+});
